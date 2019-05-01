8e3acbc5cb5ad1201414b3bdfc7724046699eb56
ukejee <ukejee3@gmail.com>
2019-05-01 07:25:35 +0100

    Login as page done and I changed the bakground color

---
+++ app/src/main/java/com/test/wourtonapp/LoginAsActivity.java
@@ -0,0 +1,21 @@
+package com.test.wourtonapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginAsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_as);
+    }
+
+    public void onClickGuest(View view){
+
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+}
